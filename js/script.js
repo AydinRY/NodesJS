15633626f2b17dc07ea34c1811767410815a9dc1
Aydin Yusubov <aydinyusubov@Aydins-MacBook-Air.local>
2021-12-14 16:56:54 +0400

    m

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("opennav").addEventListener("click", function(){
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.6)"
+})
+
+document.getElementById("closebtn").addEventListener("click", function(){
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+})
